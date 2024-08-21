87819647a65eeced7cdec3ee0723049e51971548
Dangoxy <ammar3456@yahoo.com>
2024-08-21 12:58:42 +0300

    add project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <router-link
+    :to="props.toPath"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    class="rounded-[8px]"
+  >
+    <!-- <h1>{{ props.title }}</h1> -->
+    <div
+      :class="`relative box-border size-64 rounded-[8px] border-4 border-transparent`"
+      :style="{
+        background: `url(${hover ? props.pathToGif : props.pathToImg})`,
+        backgroundSize: '100% 100%',
+      }"
+    >
+      <h1
+        class="poppins absolute bottom-0 w-full bg-emerald-100 bg-opacity-100 p-4 text-center text-[20px] font-bold text-[#222222] backdrop-blur-md"
+      >
+        {{ props.title }}
+      </h1>
+      <h1
+        class="poppins absolute left-0 top-0 flex size-12 items-center justify-center rounded-br-[4px] bg-emerald-100 bg-opacity-100 text-center text-[20px] font-bold text-[#222222] backdrop-blur-md"
+      >
+        {{ props.projectNumber }}
+      </h1>
+      <div
+        class="absolute -left-1 -top-1 size-64 rounded-[8px] border-4 border-emerald-100"
+      ></div>
+    </div>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps([
+  "projectNumber",
+  "title",
+  "pathToGif",
+  "pathToImg",
+  "toPath",
+]);
+
+let hover = ref(false);
+
+console.log(props);
+</script>
+
+<style>
+.poppins {
+  font-family: "Poppins", sans-serif;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,21 +1,27 @@
 <template>
-  <div className="flex h-screen w-full flex-col bg-[#161616] text-[#f0f0f0]">
+  <div
+    className="flex min-h-screen w-full flex-col bg-[#161616] text-[#f0f0f0]"
+  >
     <Navbar />
     <div
-      className="flex h-full w-full flex-row flex-wrap items-center justify-center gap-2 p-2"
+      className="flex h-full  pt-[62px] min-h-screen w-full flex-row items-center justify-center gap-2 p-2"
     >
-      <router-link
-        className="w-fit rounded-[8px] border-2 border-[#f0f0f0] px-4 py-2 text-[16px] font-medium text-[#f0f0f0]"
-        to="/01-Whac-a-mole"
-      >
-        01) Whac-a-mole
-      </router-link>
+      <div class="flex flex-row flex-wrap items-center justify-center gap-2">
+        <ProjectCard
+          projectNumber="01"
+          title="Whac-a-mole"
+          pathToGif="../../public/Whac-a-mole.gif"
+          pathToImg="../../public/Whac-a-mole.png"
+          toPath="/01-Whac-a-mole"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from "../components/Navbar.vue";
+import ProjectCard from "../components/ProjectCard.vue";
 </script>
 
 <style></style>
