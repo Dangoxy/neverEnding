d9a6445cdb9c5fc4e5886a1157dd7fac2f41db84
Dangoxy <ammar3456@yahoo.com>
2024-08-21 13:06:52 +0300

    changed project1 to be named

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Homepage from "./views/Homepage.vue";
-import Project1 from "./views/Project1.vue";
+import P01WhacAMole from "./views/P01WhacAMole.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { name: "home", path: "/", component: Homepage },
-    { name: "p1", path: "/01-Whac-a-mole", component: Project1 },
+    { name: "p1", path: "/01-Whac-a-mole", component: P01WhacAMole },
   ],
 });
 export default router;
