d533d541863643089886477690990f4e7cea69a4
Dangoxy <ammar3456@yahoo.com>
2024-08-21 12:57:43 +0300

    made navbar fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="font-merri w-full bg-[#161616] px-4 py-2 text-[#f0f0f0]"
+    class="font-merri fixed z-20 h-[52px] w-full bg-[#161616] bg-opacity-75 px-4 py-2 text-[#f0f0f0] backdrop-blur-lg"
     :style="{ boxShadow: '0px 0px 2px 2px rgba(0,0,0,0.75)' }"
   >
     <router-link to="/" className="text-[24px] font-medium">
