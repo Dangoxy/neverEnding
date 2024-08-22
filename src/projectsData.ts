9c66442fe3c12f3af1b2df8a2f75e966f7ec00bf
Dangoxy <ammar3456@yahoo.com>
2024-08-22 13:15:25 +0300

    refactored files

--- src/projectsData.ts
+++ src/projectsData.ts
@@ -2,22 +2,22 @@ let projectsData = [
   {
     projectNumber: "01",
     title: "Whac-a-mole",
-    pathToGif: "../../public/Whac-a-mole.gif",
-    pathToImg: "../../public/Whac-a-mole.png",
+    pathToGif: "../../public/Whac-a-mole/Whac-a-mole.gif",
+    pathToImg: "../../public/Whac-a-mole/Whac-a-mole.png",
     toPath: "/01-Whac-a-mole",
   },
   {
     projectNumber: "02",
     title: "Login form",
-    pathToGif: "../../public/LoginForm.gif",
-    pathToImg: "../../public/LoginForm.png",
+    pathToGif: "../../public/LoginForm/LoginForm.gif",
+    pathToImg: "../../public/LoginForm/LoginForm.png",
     toPath: "/02-Login-form",
   },
   {
     projectNumber: "03",
     title: "Mouse effect",
-    pathToGif: "../../public/MouseEffect.gif",
-    pathToImg: "../../public/MouseEffect.png",
+    pathToGif: "../../public/MouseEffect/MouseEffect.gif",
+    pathToImg: "../../public/MouseEffect/MouseEffect.png",
     toPath: "/03-Mouse-effect",
   },
 ];
