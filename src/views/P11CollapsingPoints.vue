08d9612e587e96b861273fdb0fc06b345bfddc09
Dangoxy <ammar3456@yahoo.com>
2024-09-01 23:59:30 +0300

    asd

--- src/views/P11CollapsingPoints.vue
+++ src/views/P11CollapsingPoints.vue
@@ -36,7 +36,7 @@
             :id="'par' + index"
             class="z-10 overflow-hidden opacity-100 transition-all duration-500"
           >
-            {{ i }}
+            {{ i + "asd" }}
           </p>
           <p
             :id="'parinvis' + index"
